c4995423a263c931b3652902f65fad26309bce40
PawanChoudhary-batch57 <pawanchoudhary1989@gmail.com>
2022-12-19 02:30:11 +0530

    code added

--- Server.js
+++ Server.js
@@ -50,7 +50,7 @@ app.get('*',function(req,res){
 })
 
 
-const port=process.env.PORT||8080
+const port=process.env.PORT||3000
 
 app.listen(port,()=>{
     console.log(`Server is running on port${port}`)
